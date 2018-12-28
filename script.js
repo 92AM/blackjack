2ae3e33cfdeb5ea01371ac067bc90b957c5ec855
Arka Mitra <arka2k8@gmail.com>
2018-12-28 23:11:52 +0000

    Added some UI elements and configured script.js control these
    Signed-off-by: Arka Mitra <arka2k8@gmail.com>

--- script.js
+++ script.js
@@ -1,32 +1,38 @@
 // Blackjack by Arka Mitra
 
-console.log("Welcome to Backjack!");
+// Card variable
+const suits = ["Hearts", "Clubs", "Diamonds", "Spades"],
+      values = ["Ace", "King", "Queen", "Jack",
+      "Ten", "Nine", "Eight", "Seven",
+      "Six", "Five", "Four", "Three", 
+      "Two"];
 
-const suits = ["Hearts", "Clubs", "Diamonds", "Spades"];
+// DOM variables (needed for UI control)
+let textArea = document.getElementById('text-area');
+let newGameButton = document.getElementById('new-game-button');
+let hitMeButton = document.getElementById('hit-me-button');
+let stayButton = document.getElementById('stay-button');
 
-const values = ["Ace", "King", "Queen", "Jack",
-    "Ten", "Nine", "Eight", "Seven",
-    "Six", "Five", "Four", "Three", "Two"];
+intialPageSetup();
 
 let deckOfCards = createDeckOfCards();
 let playerCards = [getNextCard(), getNextCard()];
 
-// For dev purpose, print the full deck of cards
-// deckOfCards.forEach(card => {
-//     console.log(card)
-// });
-
 console.log("You were dealt.");
-console.log("First card  : " + playerCards[0]);
-console.log("Second card : " + playerCards[1]);
+console.log("First card  : " + getCardAsString(playerCards[0]));
+console.log("Second card : " + getCardAsString(playerCards[1]));
 
-// All function definitions
+// All function declarations and event handlers
 
 function createDeckOfCards() {
     let deckOfCards = [];
-    suits.forEach(suit => {
-        values.forEach(value => {
-            deckOfCards.push(value + " of " + suit);
+    suits.forEach(cardSuit => {
+        values.forEach(cardValue => {
+            let card = {
+                suit: cardSuit,
+                value: cardValue
+            };
+            deckOfCards.push(card);
         })
     });
     return deckOfCards;
@@ -35,3 +41,19 @@ function createDeckOfCards() {
 function getNextCard() {
     return deckOfCards.shift();
 }
+
+function getCardAsString(card) {
+    return card.value + " of " + card.suit;
+}
+
+function intialPageSetup() {
+    hitMeButton.style.display = 'none';
+    stayButton.style.display = 'none';
+}
+
+newGameButton.addEventListener('click', function() {
+    textArea.innerText = 'New Game Started !';
+    newGameButton.style.display = 'none';
+    hitMeButton.style.display = 'inline';
+    stayButton.style.display = 'inline';
+});
