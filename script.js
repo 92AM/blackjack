3976d68d5e9c23dd8de35f1f3b79071aeeadc050
Arka Mitra <arka2k8@gmail.com>
2018-12-28 22:01:51 +0000

    Add changes to log more details
    Signed-off-by: Arka Mitra <arka2k8@gmail.com>

--- script.js
+++ script.js
@@ -8,6 +8,20 @@ const values = ["Ace", "King", "Queen", "Jack",
     "Ten", "Nine", "Eight", "Seven",
     "Six", "Five", "Four", "Three", "Two"];
 
+let deckOfCards = createDeckOfCards();
+let playerCards = [getNextCard(), getNextCard()];
+
+// For dev purpose, print the full deck of cards
+// deckOfCards.forEach(card => {
+//     console.log(card)
+// });
+
+console.log("You were dealt.");
+console.log("First card  : " + playerCards[0]);
+console.log("Second card : " + playerCards[1]);
+
+// All function definitions
+
 function createDeckOfCards() {
     let deckOfCards = [];
     suits.forEach(suit => {
@@ -18,9 +32,6 @@ function createDeckOfCards() {
     return deckOfCards;
 }
 
-const deckOfCards = createDeckOfCards();
-
-// For dev purpose, print the full deck of cards
-deckOfCards.forEach(card => {
-    console.log(card)
-});
+function getNextCard() {
+    return deckOfCards.shift();
+}
