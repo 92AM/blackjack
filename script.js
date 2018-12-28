d167cfd3bb8bd1c9d8c6883e0367df6d111a88b1
Arka Mitra <arka2k8@gmail.com>
2018-12-28 21:23:50 +0000

    Add full deck of cards into array
    Signed-off-by: Arka Mitra <arka2k8@gmail.com>

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
-// Code goes here
+// Blackjack by Arka Mitra
 
+console.log("Welcome to Backjack!");
+
+let suits = ["Hearts", "Clubs", "Diamonds", "Spades"];
+
+let values = ["Ace", "King", "Queen", "Jack",
+    "Ten", "Nine", "Eight", "Seven",
+    "Six", "Five", "Four", "Three", "Two"];
+
+let deckOfCards = [];
+
+suits.forEach(suit => {
+    values.forEach(value => {
+        deckOfCards.push(value + " of " + suit);
+    })
+});
+
+// For dev purpose, print the values
+
+deckOfCards.forEach(card => {
+    console.log(card)
+});
